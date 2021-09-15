ccc85e51f6ad911829dc2ee5418c8310a18150f0
shayaan2000 <shayaan.farooq@tintash.com>
2021-09-15 10:51:41 +0500

    Redux persistance added to cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-export default combineReducers({
+
+// configs for persist
+const persistConfig = {
+  key: "root", //where we want to start in reducer
+  storage,
+  //list of reducers we want to store
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import persistStore from "redux-persist/es/persistStore";
 import rootReducer from "./root-reducer";
 
 // we store middlewares in an array
 const middlewares = [logger];
 
 //store
-const store = createStore(rootReducer, applyMiddleware(...middlewares)); //spreading middleware so all in array are sent
+export const store = createStore(rootReducer, applyMiddleware(...middlewares)); //spreading middleware so all in array are sent
 
-export default store;
+// for persistance
+export const persistor = persistStore(store);
+
+// making a variable to avoid warning
+const exp = { store, persistor };
+export default exp;
